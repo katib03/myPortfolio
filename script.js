6ca827780c7b694e682cf6662c61657605730849
katib03 <ibkocak03@gmail.com>
2023-01-11 22:49:18 +0300

    modified html, css and js files

--- script.js
+++ script.js
@@ -4,9 +4,25 @@ const prj3 = document.querySelector('#prj3');
 const project1 = document.querySelector('#project1');
 const project2 = document.querySelector('#project2');
 const project3 = document.querySelector('#project3');
+const old = document.querySelector('#old');
 
-
-
-prj1.addEventListener('toggle', () => {
+prj1.addEventListener('click', () => {
   project1.style.display = "block";
+  project2.style.display = "none";
+  project3.style.display = "none";
+});
+prj2.addEventListener('click', () => {
+  project1.style.display = "none";
+  project2.style.display = "block";
+  project3.style.display = "none";
+});
+prj3.addEventListener('click', () => {
+  project1.style.display = "none";
+  project2.style.display = "none";
+  project3.style.display = "block";
+});
+old.addEventListener('click', () => {
+  project1.style.display = "none";
+  project2.style.display = "none";
+  project3.style.display = "none";
 });
