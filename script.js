a202dc6b6b758a96f64aaec6c907843b6ba3bff9
katib03 <ibkocak03@gmail.com>
2023-01-11 22:38:38 +0300

    connect to js file. link to second page and other detail css

---
+++ script.js
@@ -0,0 +1,12 @@
+const prj1 = document.querySelector('#prj1');
+const prj2 = document.querySelector('#prj2');
+const prj3 = document.querySelector('#prj3');
+const project1 = document.querySelector('#project1');
+const project2 = document.querySelector('#project2');
+const project3 = document.querySelector('#project3');
+
+
+
+prj1.addEventListener('toggle', () => {
+  project1.style.display = "block";
+});
